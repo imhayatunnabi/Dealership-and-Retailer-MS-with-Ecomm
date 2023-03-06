e68fef9d47277f26f9695ff52dd6182e80286121
imhayatunnabi <nabilahmednafiahmed753@gmail.com>
2023-03-06 17:38:34 +0600

    Banner crud routes and validation rules done

--- app/Models/Banner.php
+++ app/Models/Banner.php
@@ -2,11 +2,21 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Contracts\Console\Application;
+use Illuminate\Contracts\Routing\UrlGenerator;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Banner extends Model
 {
     use HasFactory;
     protected $guarded =[];
+    public function getImageAttribute($value): string|UrlGenerator|Application
+    {
+        if($value){
+            return Storage::url('banner/'.$value);
+        }
+        return url('public/images/user.jpg');
+    }
 }
--- resources/views/backend/pages/banner/index.blade.php
+++ resources/views/backend/pages/banner/index.blade.php
@@ -6,8 +6,9 @@
         <thead>
             <tr>
                 <th scope="col">Serial</th>
-                <th scope="col">Name</th>
                 <th scope="col">Image</th>
+                <th scope="col">Heading</th>
+                <th scope="col">Banner Url</th>
                 <th scope="col">Action</th>
             </tr>
         </thead>
@@ -15,13 +16,13 @@
             @foreach ($banners as $key=>$item)
             <tr class="">
                 <td scope="row">{{ $key }}</td>
+                <td><img width="60px" src="{{ $item->image }}" alt="{{ $item->heading }}" srcset=""></td>
                 <td>{{ $item->heading }}</td>
                 <td>{{ $item->banner_url }}</td>
-                <td>{{ $item->image }}</td>
                 <td>
                     <a href="{{ route('backend.banner.edit',$item->id) }}" class="btn btn-primary"><i
                             class="fa fa-pencil-square" aria-hidden="true"></i></a>
-                    <a href="{{ route('backend.banner.destroy',$item->id) }}" class="btn btn-primary"><i
+                    <a href="{{ route('backend.banner.destroy',$item->id) }}" class="btn btn-danger"><i
                             class="fa fa-trash" aria-hidden="true"></i></a>
                 </td>
             </tr>
